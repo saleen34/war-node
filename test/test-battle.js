b737bc519bc001ff503d22045d307865590c06b8
Nick Pate <saleen34@gmail.com>
2017-09-23 19:52:51 -0700

    Fixed broken "bottom" deck playing

--- test/test-battle.js
+++ test/test-battle.js
@@ -29,8 +29,8 @@ describe('Battle', () => {
       p1.populateHand(deck.half1);
 
       // get last element in cards to pretend to pop it
-      const val0 = p0.cards[p0.cards.length - 1].value;
-      const val1 = p1.cards[p1.cards.length - 1].value;
+      const val0 = p0.cards[0].value;
+      const val1 = p1.cards[0].value;
 
       const battle = new Battle(p0, p1);
       battle.fight();
@@ -38,11 +38,11 @@ describe('Battle', () => {
       if (val0 > val1) {
         assert.equal(p0.countCards(), 27);
         assert.equal(p1.countCards(), 25);
-      } else if (val1 > val0) {
+      }
+
+      if (val1 > val0) {
         assert.equal(p1.countCards(), 27);
         assert.equal(p0.countCards(), 25);
-      } else {
-        console.log('tbd');
       }
     });
 
@@ -55,9 +55,9 @@ describe('Battle', () => {
       const p1 = new Player('playerTwo');
       p1.populateHand(deck.half1);
 
-      // get last element in cards to pretend to pop it
-      const val0 = p0.cards[p0.cards.length - 1].value;
-      const val1 = p1.cards[p1.cards.length - 1].value;
+      // get first element in cards (top card)
+      const val0 = p0.cards[0].value;
+      const val1 = p1.cards[0].value;
 
       const battle = new Battle(p0, p1);
       battle.fight();
@@ -65,12 +65,11 @@ describe('Battle', () => {
       if (val0 > val1) {
         assert.equal(battle.getWinner().name, 'playerOne');
         assert.equal(battle.getLoser().name, 'playerTwo');
-      } else if (val1 > val0) {
+      }
+
+      if (val1 > val0) {
         assert.equal(battle.getWinner().name, 'playerTwo');
         assert.equal(battle.getLoser().name, 'playerOne');
-      } else {
-        // tbd
-        console.log('tbd');
       }
     });
   });
